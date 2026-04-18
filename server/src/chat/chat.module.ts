f3ee3589502fee1b92dc4f9e41f39ff473a13426
jet-isnt-haha <1802966308@qq.com>
2026-04-18 20:39:08 +0800

    refactor(research): extract independent research agent layer

--- server/src/chat/chat.module.ts
+++ server/src/chat/chat.module.ts
@@ -3,9 +3,15 @@ import { ChatController } from './chat.controller';
 import { ChatService } from './chat.service';
 import { InMemoryChatRepository } from './chat.repository';
 import { TavilyResearchSearchAdapter } from './research-search.adapter';
+import { ResearchAgentService } from './research-agent.service';
 
 @Module({
   controllers: [ChatController],
-  providers: [ChatService, InMemoryChatRepository, TavilyResearchSearchAdapter],
+  providers: [
+    ChatService,
+    ResearchAgentService,
+    InMemoryChatRepository,
+    TavilyResearchSearchAdapter,
+  ],
 })
 export class ChatModule {}
